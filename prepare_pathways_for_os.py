6dd04d645273c6cf113342a938c10b37b58d930f
Ethan <ethanpsimons38@gmail.com>
2021-07-23 12:28:19 -0500

    Update prepare_pathways_for_os.py
    Update

--- prepare_pathways_for_os.py
+++ prepare_pathways_for_os.py
@@ -1,11 +1,9 @@
+import os
 import sys
 
 # pathway separator
 # Choose seperator type based on whether or not
 # system is Linux or Windows
 chk_os = platform.system()
-if chk_os == "Windows":
-    sep = "\\"
-else:
-    sep = "/"
+sep = os.path.sep
 print("System is: {}\nSeparator is: {}".format(chk_os, sep))
